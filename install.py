2699d1bb4f69c5abca5d8176cb1007e34eaae506
Marcel <info@ms-labs.de>
2026-02-27 10:47:52 +0100

    init

---
+++ install.py
@@ -0,0 +1,25 @@
+# installer for wetterblick.com
+# Copyright 2014-2020 Matthew Wall + MSlabs
+# Distributed under the terms of the GNU Public License (GPLv3)
+
+from weecfg.extension import ExtensionInstaller
+
+def loader():
+    return WetterblickInstaller()
+
+class WetterblickInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(WetterblickInstaller, self).__init__(
+            version="0.1",
+            name='wetterblick',
+            description='Upload weather data to wetterblick.com.',
+            author="Matthew Wall + MSlabs",
+            author_email="",
+            restful_services='user.wetter.Wetterblick',
+            config={
+                'StdRESTful': {
+                    'Wetterblick': {
+                        'username': 'INSERT_USERNAME_HERE',
+                        'password': 'INSERT_PASSWORD_HERE'}}},
+            files=[('bin/user', ['bin/user/wetterblick.py'])]
+            )
---
+++ readme.txt
@@ -0,0 +1,25 @@
+wetterblick - weewx extension that sends data to wetterblick.com
+Adapted in 2026 by MSlabs, special thanks to Matthew Wall (2024-2020) for the original Code.
+Distributed under the terms of the GNU Public License (GPLv3)
+
+Installation instructions:
+
+1) download
+
+wget -O weewx-wetterblick.zip https://github.com/marcels97/weewx-wetterblick/archive/master.zip
+
+1) run the installer:
+
+wee_extension -install weewx-wetterblick.zip
+
+2) modify weewx.conf:
+
+[StdRESTful]
+    [[Wetter]]
+        username = USERNAME
+        password = PASSWORD
+
+3) restart weewx
+
+sudo /etc/init.d/weewx stop
+sudo /etc/init.d/weewx start
